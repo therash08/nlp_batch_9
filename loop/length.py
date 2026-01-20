91bae05abba61919532a35e1c8343e6f52ea03ef
therash45 <therash792@gmail.com>
2026-01-21 02:50:42 +0600

    Add loop practice examples

---
+++ loop/length.py
@@ -0,0 +1,7 @@
+name = "Rasidul Hoque chowdhury"
+size_name = len(name)
+
+for i in range(size_name):
+    if name[i] == ' ':
+        continue
+    print(name[i])
---
+++ loop/test.py
@@ -0,0 +1,5 @@
+# for i in range(100):
+#     print(i, " rasidul")
+
+for i in range(1, 10, 2):
+    print(i)
